58698a9a962297c4ebc802ea6178a089f39b28cb
adamchappell00 <adam.chappell00@gmail.com>
2022-04-27 09:50:52 -0500

    UserIO Exercise Complete

---
+++ src/main/java/module13/userIO/Input.java
@@ -0,0 +1,41 @@
+package module13.userIO;
+
+import module13.userIO.interfaces.UserIO;
+
+import java.util.Scanner;
+
+
+public class Input implements UserIO {
+
+    Scanner scan = new Scanner(System.in);
+
+    @Override
+    public int readInt(String prompt) {
+        System.out.println(prompt);
+        return Integer.parseInt(scan.nextLine());
+    }
+
+    @Override
+    public long readLong(String prompt) {
+        System.out.println(prompt);
+        return Long.parseLong(scan.nextLine());
+    }
+
+    @Override
+    public double readDouble(String prompt) {
+        System.out.println(prompt);
+        return Double.parseDouble(scan.nextLine());
+    }
+
+    @Override
+    public float readFloat(String prompt) {
+        System.out.println(prompt);
+        return Float.parseFloat(scan.nextLine());
+    }
+
+    @Override
+    public String readString(String prompt) {
+        System.out.println(prompt);
+        return scan.nextLine();
+    }
+}
