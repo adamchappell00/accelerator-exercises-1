11a8dce26262899408a2f8505f449615c00c0754
adamchappell00 <adam.chappell00@gmail.com>
2022-04-28 15:47:44 -0500

    List Exercise and Tests Complete

--- src/main/java/module14/usinglists/App.java
+++ src/main/java/module14/usinglists/App.java
@@ -1,8 +1,11 @@
 package module14.usinglists;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class App {
 
-    public static int total (int[] numbers) {
+    public static int total (List<Integer> numbers) {
 
         int sum = 0;
         for(int num : numbers) {
@@ -12,63 +15,60 @@ public class App {
         return sum;
     }
 
-    public static int totalEven (int[] numbers) {
+    public static int totalEven (List<Integer> numbers) {
 
         int sum = 0;
-        for(int i = 0; i < numbers.length; i += 2) {
-            sum += numbers[i];
+        for(int i = 0; i < numbers.size(); i += 2) {
+            sum += numbers.get(i);
         }
 
         return sum;
     }
 
-    public static String[] swapFirstAndLast(String[] strings) {
+    public static List<String> swapFirstAndLast(List<String> strings) {
 
-        String temp = strings[0];
-        strings[0] = strings[ strings.length - 1 ];
-        strings[ strings.length - 1 ] = temp;
+        String temp = strings.get(0);
+        strings.set(0, strings.get(strings.size() - 1));
+        strings.set(strings.size() - 1, temp);
 
         return strings;
     }
 
-    public static int[] reverse(int[] numbers) {
+    public static List<Integer> reverse(List<Integer> numbers) {
 
-        int[] reversed = new int[numbers.length];
+        List<Integer> reversed = new ArrayList<>();
 
-        for(int i = 0; i < numbers.length; i++) {
+        for(int i = numbers.size()-1; i >= 0; i--) {
 
             // length - (i + 1) is the n-th to last element
             // so when i = 0, it would be the last element
             // when i = 3, it would be the fourth to last element since i=3 is the 4th element, etc
-            reversed[i] = numbers[ numbers.length - (i + 1) ];
+            reversed.add(numbers.get(i));
         }
 
         return reversed;
     }
 
-    public static int[] lessThanFive(int[] numbers) {
+    public static List<Integer> lessThanFive(List<Integer> numbers) {
 
-        int numLessThanFive = 0;
-
-        for(int num : numbers) {
-            if ( num < 5 ) {
-                numLessThanFive++;
+        int lessThanFive = 0;
+        for(int num : numbers){
+            if(num < 5){
+                lessThanFive++;
             }
         }
 
-        if ( numLessThanFive == 0 ) {
+        if(lessThanFive == 0){
             return null;
         }
-
-        int[] lessThan = new int[numLessThanFive];
+        List<Integer> lessThan = new ArrayList<>();
 
         for(int num : numbers) {
             if ( num < 5 ) {
 
                 // subtracting numLessThanFive from length then decrementing numLessThanFive
                 // allows us to go from 0 to length - 1 in order without additional variables
-                lessThan[lessThan.length - numLessThanFive] = num;
-                numLessThanFive--;
+               lessThan.add(num);
             }
         }
 
@@ -76,21 +76,10 @@ public class App {
     }
 
     //challenge
-    public static int[][] splitAtFive(int[] numbers) {
-        int numLessThanFive = 0;
-        int numMoreThanFive = 0;
+    public static List<List<Integer>> splitAtFive(List<Integer> numbers) {
 
-        for(int num : numbers) {
-            if ( num < 5 ) {
-                numLessThanFive++;
-            } else {
-                numMoreThanFive++;
-            }
-        }
-
-        int[] lessThan = new int[numLessThanFive];
-        int[] moreThan = new int[numMoreThanFive];
-
+        List<Integer> lessthan = new ArrayList<>();
+        List<Integer> morethan = new ArrayList<>();
 
         for(int num : numbers) {
 
@@ -98,44 +87,37 @@ public class App {
             // allows us to go from 0 to length - 1 in order without additional variables
             // same with numMoreThanFive
             if ( num < 5 ) {
-                lessThan[lessThan.length - numLessThanFive] = num;
-                numLessThanFive--;
+                lessthan.add(num);
             } else {
-                moreThan[moreThan.length - numMoreThanFive] = num;
-                numMoreThanFive--;
+                morethan.add(num);
             }
         }
-
-        return new int[][] { lessThan, moreThan };
+        List<List<Integer>> listOfLists = new ArrayList<>();
+        listOfLists.add(lessthan);
+        listOfLists.add(morethan);
+        return listOfLists;
     }
 
     // Challenge
-    public static String[][] evensAndOdds(String[] strings) {
+    public static List<List<String>> evensAndOdds(List<String> strings) {
 
         //leveraging integer division
-        String[] odds = new String[ strings.length/2 ];
+        List<String> odds = new ArrayList<>();
 
         // Set evens to null for reassignment below
-        String[] evens = null;
+        List<String> evens = new ArrayList<>();
 
-        // again leveraging integer division
-        // if it's already of even length, we're good
-        // but if it's of odd length, there's one more even index than odd
-        if (strings.length % 2 == 0) {
-            evens = new String[ strings.length/2];
-        } else {
-            evens = new String[ strings.length/2 + 1];
-        }
-
-        for(int i = 0; i < strings.length; i++) {
-            int currIndex = i/2;
-            if( i % 2 == 0 ) {
-                evens[currIndex] = strings[i];
+        for(String string : strings){
+            if(strings.indexOf(string) % 2 == 0){
+                evens.add(string);
             } else {
-                odds[currIndex] = strings[i];
+                odds.add(string);
             }
         }
+        List<List<String>> listOfLists = new ArrayList<>();
+        listOfLists.add(evens);
+        listOfLists.add(odds);
 
-        return new String[][] { evens, odds };
+        return listOfLists;
     }
 }
